fa4ac646a4601b149c1b843fc79894bfbeb26520
Artur de Maria <arturdemaria7@gmail.com>
2025-03-26 05:36:55 -0300

    [UPDATE] add online api URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // URL do backend
+  baseURL: "https://projeto-final-capacita.onrender.com", // URL do backend
   withCredentials: true,
 });
 
